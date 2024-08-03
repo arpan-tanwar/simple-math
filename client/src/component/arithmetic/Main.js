cbb43be85d2b68ee6632497fc672f96ab3400191
Arpan Tanwar <arpantanwar.at@gmail.com>
2024-08-03 18:56:33 -0400

    Added library

--- client/src/component/arithmetic/Main.js
+++ client/src/component/arithmetic/Main.js
@@ -1,19 +1,16 @@
-import React, { useState } from 'react'
-import Youtube from './Youtube'
-import DrillSection from './DrillSection'
-import Arithmetic from './arithmetic/Arithmetic'
-
+import React, { useState } from "react";
+import Youtube from "./Youtube";
+import DrillSection from "./DrillSection";
+import Arithmetic from "./arithmetic/Arithmetic";
 
 const Main = () => {
-  const [component, setComponent] = useState(1)
+  const [component, setComponent] = useState(1);
   return (
-    <div className='mt-20 '>
-      <Youtube/>
-      {component===1 &&    <Arithmetic/>}
-    
-   
+    <div className="mt-20 ">
+      <Youtube />
+      {component === 1 && <Arithmetic />}
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
